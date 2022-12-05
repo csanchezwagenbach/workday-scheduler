25b4e1e44c0df1a507bf9a40256b6ea7711d4b6d
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2022-12-05 14:43:58 -0800

    added in current date and time into header using jquery

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var dateAndTime = $("#currentDay")
+function displayDateAndTime () {
+var nowMoment = dayjs().format("MMM DD, YYYY [at] hh:mm:ss a");
+dateAndTime.text(nowMoment);
+}
 $(function () {
+
+  // function showNow () {nowMoment.text(dayjs().format("MMM DD, YYYY hh:mm:ss a"))};
+  // setInterval(showNow(), 1000);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -19,5 +27,6 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+displayDateAndTime();
+setInterval(displayDateAndTime, 1000);
 });
