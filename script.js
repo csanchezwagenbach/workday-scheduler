3e9d3d9e7808631ce9582911f7b5e6ed94219d32
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2022-12-06 09:51:55 -0800

    successfully pushing objects into local storage, successfully pulling objects out of local storage, next step is assigning values pulled from local storage back onto web page

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ var hourSeventeen = {
   ];
 
    function saveProjects(event) {
-    console.log(projects)
+
      event.preventDefault();
 
      var hourNine = {
@@ -144,25 +144,8 @@ var hourSeventeen = {
    saveButton.on("click", saveProjects);
 
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-
  for (var i=0; i < projects.length; i++) {
  var parentBlock = $(projects[i].project.parent());
-// console.log(parentBlock);
-// console.log(projects[i].hour.valueOf());
-// console.log(dayjs().format("HH"));
      if (projects[i].hour.valueOf() - dayjs().format("HH") > 0) {
       parentBlock.removeClass("past");
       parentBlock.addClass("future");
@@ -170,10 +153,26 @@ var hourSeventeen = {
       parentBlock.removeClass("past");
       parentBlock.addClass("present");
   }
+ }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-displayDateAndTime();
-setInterval(displayDateAndTime, 1000);
- }});
+ function getProjects () {
+  var projectsFromStorage = localStorage.getItem("projects");
+  if (projectsFromStorage) {
+    projectsFromStorage = JSON.parse(projectsFromStorage);
+  } else {
+    return projectsFromStorage;
+  }
+  console.log(projectsFromStorage);
+ }
+  
+  
+
+
+ 
+ displayDateAndTime();
+ setInterval(displayDateAndTime, 1000);
+ getProjects();
+});
