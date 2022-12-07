764cb4edd1476bad8decdf0550aa92201562c2fe
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2022-12-07 10:02:11 -0800

    deleted initial comments, added personal comments throughout js file

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 
 $(function () {
+  //To begin with, I call a reference to the HTML element where the current date and time will be displayed, then, I use dayjs to set the textContent of that element to be the current date and time.
   var dateAndTime = $("#currentDay")
 
   function displayDateAndTime () {
   var nowMoment = dayjs().format("MMM DD, YYYY [at] hh:mm:ss a");
   dateAndTime.text(nowMoment);
   }
+
+  //Here I call references to the primary HTML elements I want to be interacting with, the save buttons and the text input areas next to each time block
 
   var saveButton = $(".saveBtn");
   var hourNineInput = $("#hour-9-project");
@@ -22,63 +21,67 @@ $(function () {
   var hourSixteenInput = $("#hour-16-project");
   var hourSeventeenInput = $("#hour-17-project");
 
-var hourNine = {
-  project: hourNineInput,
-  text: hourNineInput.val(),
-  hour: 9,
-};
+  //I collect each crucial aspect of the projects into an object for each hour block, then I collect those objects into an array just below.
 
-var hourTen = {
-  project: hourTenInput,
-  text: hourTenInput.val(),
-  hour: 10
-};
+  var hourNine = {
+    project: hourNineInput,
+    text: hourNineInput.val(),
+    hour: 9,
+  };
 
-var hourEleven = {
-  project: hourElevenInput,
-  text: hourElevenInput.val(),
-  hour: 11
-};
+  var hourTen = {
+    project: hourTenInput,
+    text: hourTenInput.val(),
+    hour: 10
+  };
 
-var hourTwelve = {
-  project: hourTwelveInput,
-  text: hourTwelveInput.val(),
-  hour: 12
-};
+  var hourEleven = {
+    project: hourElevenInput,
+    text: hourElevenInput.val(),
+    hour: 11
+  };
 
-var hourThirteen = {
-  project: hourThirteenInput,
-  text: hourThirteenInput.val(),
-  hour: 13
-};
+  var hourTwelve = {
+    project: hourTwelveInput,
+    text: hourTwelveInput.val(),
+    hour: 12
+  };
 
-var hourFourteen = {
-  project: hourFourteenInput,
-  text: hourFourteenInput.val(),
-  hour: 14,
-};
+  var hourThirteen = {
+    project: hourThirteenInput,
+    text: hourThirteenInput.val(),
+    hour: 13
+  };
 
-var hourFifteen = {
-  project: hourFifteenInput,
-  text: hourFifteenInput.val(),
-  hour: 15,
-};
+  var hourFourteen = {
+    project: hourFourteenInput,
+    text: hourFourteenInput.val(),
+    hour: 14,
+  };
 
-var hourSixteen = {
-  project: hourSixteenInput,
-  text: hourSixteenInput.val(),
-  hour: 16
-};
+  var hourFifteen = {
+    project: hourFifteenInput,
+    text: hourFifteenInput.val(),
+    hour: 15,
+  };
 
-var hourSeventeen = {
-  project: hourSeventeenInput,
-  text: hourSeventeenInput.val(),
-  hour: 17
-};
+  var hourSixteen = {
+    project: hourSixteenInput,
+    text: hourSixteenInput.val(),
+    hour: 16
+  };
+
+  var hourSeventeen = {
+    project: hourSeventeenInput,
+    text: hourSeventeenInput.val(),
+    hour: 17
+  };
 
   var projects = [
     hourNine, hourTen, hourEleven, hourTwelve, hourThirteen, hourFourteen, hourFifteen, hourSixteen, hourSeventeen
   ];
+
+//Here is the function that runs once the save buttons have been clicked. I ran into the issue of needing to copy the objects and arrays I had written above so that they would properly run during the event listener. A possible source of confusion to a reader, but to be clear, these objects and this array are two separate ones, one scoped globally, the other within the event listener.
 
    function saveProjects(event) {
 
@@ -135,13 +138,13 @@ var hourSeventeen = {
 
      for (var i = 0; i < projects.length; i++) {  
       var projectText = projects[i].text;  
-      console.log(projectText)
-  };  
+    };  
    localStorage.setItem("projects", JSON.stringify(projects));
    }
 
    saveButton.on("click", saveProjects);
 
+//Here, I loop through the parent elements of each hour-block text area. Each object was given an "hour" property above, with its value being the hour in military format. I loop through the objects and compare the value of the hour property with the current time from dayjs. Based on the difference of those two values, I know whether a given hour block is in the past, present or future. I set each block to "past" as a default, and set two situations under which past is removed, and the appropriate class added.
 
  for (var i=0; i < projects.length; i++) {
  var parentBlock = $(projects[i].project.parent());
@@ -153,10 +156,9 @@ var hourSeventeen = {
       parentBlock.addClass("present");
   }
  }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  
+
+//Here, I pull whatever is stored in local storage and set the initial text values of the hour-block input areas to be equal to whatever was identically indexed in local storage. I used this fact a lot, that because there are a finite amount of hours, I could pretty seamlessly set things to the same index and populate/save data accordingly 
+
  function getProjects () {
   var projectsFromStorage = localStorage.getItem("projects");
   if (projectsFromStorage) {
@@ -164,11 +166,11 @@ var hourSeventeen = {
   } else {
     return projectsFromStorage;
   }
- // console.log(projectsFromStorage)
+
 
   for (var i = 0; i < projectsFromStorage.length; i++) {
     projects[i].project.val(projectsFromStorage[i].text);
-    console.log(projects[i].project.textContent)
+    
   }
  } 
   
